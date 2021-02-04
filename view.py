053904962eff36f1a10c635dd0a5eedff36372e5
Leonardo Germán Loza Bonora <leonardogermanlozabonora@gmail.com>
2021-02-03 21:26:38 -0300

    Improves visuals of the app
    The right clock and the stop buttons logic has not
    been programmed yet.

--- view.py
+++ view.py
@@ -4,12 +4,38 @@ class View():
     def __init__(self, root):
         self.root = root
         root.title("Animedoro-app")
-        root.geometry("400x200")
+        root.geometry("600x200")
+        root.resizable(False,False)
+        root.configure(bg="#393A3A")
+        self.createLeftTimer()
+        self.createRightTimer()
+        
+    def createLeftTimer(self):
+        leftFrame = tk.Frame(self.root, width=200, height=200)
+        leftFrame.pack(side=tk.LEFT)
+        leftFrame.pack_propagate(0)
 
-        self.timerLabel = tk.Label()
-        self.timerStart = tk.Button(text="Start")
-        self.timerReset = tk.Button(text="Reset")
+        self.leftTimerLabel = tk.Label(leftFrame, background="black", fg="green", font=("Roboto", 20, "bold"))
+        self.leftTimerStart = tk.Button(leftFrame, text="Start", background="green", fg="white", font=("Roboto", 10))
+        self.leftTimerStop = tk.Button(leftFrame, text="Stop", background="red", fg="white", font=("Roboto", 10))
+        self.leftTimerReset = tk.Button(leftFrame, text="Reset", background="gray", fg="white", font=("Roboto", 10))
 
-        self.timerLabel.grid(column=0, row=1)
-        self.timerStart.grid(column=0, row=2)
-        self.timerReset.grid(column=0, row=3)
+        self.leftTimerLabel.pack(fill = tk.BOTH, expand = True)
+        self.leftTimerStart.pack(fill = tk.BOTH, expand = True)
+        self.leftTimerStop.pack(fill = tk.BOTH, expand = True)
+        self.leftTimerReset.pack(fill = tk.BOTH, expand = True)
+
+    def createRightTimer(self):
+        rightFrame = tk.Frame(self.root, width=200, height=200)
+        rightFrame.pack(side=tk.RIGHT)
+        rightFrame.pack_propagate(0)
+
+        self.rightTimerLabel = tk.Label(rightFrame, background="black", fg="green", font=("Roboto", 20, "bold"))
+        self.rightTimerStart = tk.Button(rightFrame, text="Start", background="green", fg="white", font=("Roboto", 10))
+        self.rightTimerStop = tk.Button(rightFrame, text="Stop", background="red", fg="white", font=("Roboto", 10))
+        self.rightTimerReset = tk.Button(rightFrame, text="Reset", background="gray", fg="white", font=("Roboto", 10))
+
+        self.rightTimerLabel.pack(fill = tk.BOTH, expand = True)
+        self.rightTimerStart.pack(fill = tk.BOTH, expand = True)
+        self.rightTimerStop.pack(fill = tk.BOTH, expand = True)
+        self.rightTimerReset.pack(fill = tk.BOTH, expand = True)
